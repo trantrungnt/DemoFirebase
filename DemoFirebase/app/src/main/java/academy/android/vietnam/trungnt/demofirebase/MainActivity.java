e2ef7482a59447ae7fdf4370bc85bedce03983dc
Trần Trung <nguyentrantrung1986@gmail.com>
2016-01-05 13:06:52 -0800

    setup Firebase

--- DemoFirebase/app/src/main/java/academy/android/vietnam/trungnt/demofirebase/MainActivity.java
+++ DemoFirebase/app/src/main/java/academy/android/vietnam/trungnt/demofirebase/MainActivity.java
@@ -3,11 +3,14 @@ package academy.android.vietnam.trungnt.demofirebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
     }
 }
