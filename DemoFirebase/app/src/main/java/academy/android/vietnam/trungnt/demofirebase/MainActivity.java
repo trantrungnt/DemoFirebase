4367ce2511f9ef4bf8865629dc66e356602c7db2
Trần Trung <nguyentrantrung1986@gmail.com>
2016-03-24 13:20:28 +0700

    config Firebase

--- DemoFirebase/app/src/main/java/academy/android/vietnam/trungnt/demofirebase/MainActivity.java
+++ DemoFirebase/app/src/main/java/academy/android/vietnam/trungnt/demofirebase/MainActivity.java
@@ -1,15 +1,14 @@
 package academy.android.vietnam.trungnt.demofirebase;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
-import com.firebase.ui.FirebaseListAdapter;
-import com.firebase.ui.auth.core.AuthProviderType;
-import com.firebase.ui.auth.core.FirebaseLoginBaseActivity;
-import com.firebase.ui.auth.core.FirebaseLoginError;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
-
-public class MainActivity extends FirebaseLoginBaseActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +17,16 @@ public class MainActivity extends FirebaseLoginBaseActivity {
         Firebase.setAndroidContext(this);
 
         Firebase myFirebaseRef = new Firebase("https://student-manage.firebaseio.com/");
+        myFirebaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue());
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
     }
+
 }
